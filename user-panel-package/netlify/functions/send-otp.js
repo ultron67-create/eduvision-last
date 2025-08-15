a7d64fc0570672c76cc8e2947532b11479ede613
ultron67 <avashtrikhatri06@gmail.com>
2025-08-15 14:40:27 +0530

    Delete user-panel-package directory

--- user-panel-package/netlify/functions/send-otp.js
+++
@@ -1,8 +0,0 @@
-export async function handler(event) {
-  const { phone } = JSON.parse(event.body);
-  console.log(`OTP requested for ${phone}`);
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "OTP sent successfully" }),
-  };
-}
