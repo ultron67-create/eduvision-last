b030c15cb9a90644ff272a0cc3053a8788fad536
ultron67 <avashtrikhatri06@gmail.com>
2025-08-16 13:27:03 +0530

    Delete eduvision_pricing_tab directory

--- eduvision_pricing_tab/src/pricing/index.js
+++
@@ -1,5 +0,0 @@
-export { default as PricingTab } from "./PricingTab";
-export { default as PricingLink } from "./PricingLink";
-export { default as NavPlanBadge } from "./NavPlanBadge";
-export { PlanProvider, usePlan } from "./PlanContext";
-export * from "./quota";
